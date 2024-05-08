33b5d21c690c8aefe972ce05c8f69a9880e53825
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-05-09 01:14:44 +0600

    applied pagination

--- ema-john-pagination-client/src/components/Shop/Shop.jsx
+++ ema-john-pagination-client/src/components/Shop/Shop.jsx
@@ -3,17 +3,41 @@ import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fa
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState([]);
+    const [itemsPerPage, setItemsPerPage] = useState(10);
+    const [currentPage, setCurrentPage] = useState(0)
+    const {count} = useLoaderData();
+
+    const numberOfPages = Math.ceil(count/itemsPerPage);
+
+  console.log("current Page" , currentPage);
+
+    //to get all pages by number
+        // const pages = [];
+        // for(let i=0; i < numberOfPages; i++){
+        //   pages.push(i);
+        // }
+    const pages = [...Array(numberOfPages).keys()]
+
+
+    /**
+     * Steps
+     * ===============
+     * 1. get the total number of products
+     * 2. number of items per page dynamic
+     * 3. total number of pages & put them in a array
+     * 4. keep current page on the state
+     */
 
     useEffect(() => {
-        fetch('http://localhost:5000/products')
+        fetch(`http://localhost:5000/products?page=${currentPage}&size=${itemsPerPage}`)
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, []);
+    }, [currentPage, itemsPerPage]);
 
     useEffect(() => {
         const storedCart = getShoppingCart();
@@ -61,6 +85,24 @@ const Shop = () => {
         deleteShoppingCart();
     }
 
+    const handleItemsPerPage = (e) => {
+      const value = parseInt(e.target.value);
+      console.log(value);
+      setItemsPerPage(value);
+      setCurrentPage(0);
+    }
+
+    const handlePrevBtn = () => {      
+      if(currentPage > 0) {
+        setCurrentPage(currentPage - 1);
+      }
+    }
+    const handleNextBtn = () => {      
+     if(currentPage < pages.length -1 ) {
+      setCurrentPage(currentPage + 1)
+    }
+    }
+
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -81,6 +123,30 @@ const Shop = () => {
                         <button className='btn-proceed'>Review Order</button>
                     </Link>
                 </Cart>
+            </div>
+
+            <div className='pagination'>
+           
+              {currentPage > 0 &&  <button onClick={handlePrevBtn}>Prev</button>}
+
+                {
+                  pages.map(page => <button 
+                    className={currentPage === page ? 'selected' : ''}
+                    onClick={() => setCurrentPage(page)}
+                    key={page}>{page}</button>)
+                }
+              {
+                currentPage < pages.length - 1 && <button onClick={handleNextBtn}>Next</button>
+              }
+
+
+
+                <select onChange={handleItemsPerPage} value={itemsPerPage} name='' id=''>
+                  <option value="5">5</option>
+                  <option value="10">10</option>
+                  <option value="20">20</option>
+                  <option value="50">50</option>
+                </select>
             </div>
         </div>
     );
--- ema-john-pagination-server/index.js
+++ ema-john-pagination-server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluste1.ommflfg.mongodb.net/?retryWrites=true&w=majority&appName=Cluste1`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,16 +23,38 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
 
     const productCollection = client.db('emaJohnDB').collection('products');
+    
+    app.get('/products', async(req, res) => {
+        const page = parseInt(req.query.page);
+        const size = parseInt(req.query.size);
+        const result = await productCollection.find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+        res.send(result);
+    })
 
-    // app.get('/products', async(req, res) => {
-    //     const result = await productCollection.find().toArray();
-    //     res.send(result);
-    // })
+    app.get("/productsCount", async(req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({count});
+    })
+
+    app.post('/productByIds', async(req, res) => {
+      const ids = req.body;
+      const idsWithObjectId = ids.map(id => new ObjectId(id));
+      const query = {_id: {$in : idsWithObjectId}}
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
+    })
 
 
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -40,6 +62,8 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) =>{
